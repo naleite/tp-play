e31e86685b794a821b22ba9a34cfb0b7b0dccc42
josian <marinier.josian@yahoo.fr>
2015-01-07 09:07:18 +0100

    Observable probleme avec stream attendee

--- app/controllers/MyAuthenticator.java
+++ app/controllers/MyAuthenticator.java
@@ -8,6 +8,5 @@ public class MyAuthenticator extends play.mvc.Security.Authenticator {
     @Override
     public play.mvc.Result onUnauthorized(play.mvc.Http.Context context){
         return redirect(routes.Authentication.login());
-
     }
 }
